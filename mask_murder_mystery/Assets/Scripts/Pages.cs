40e6b01a0650bc6d2803dd6b6db9ddd75bb2cece
Jake-2-3 <166446558+Jake-2-3@users.noreply.github.com>
2025-12-27 00:33:05 -0500

    journal

---
+++ mask_murder_mystery/Assets/Scripts/Pages.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pages : MonoBehaviour
+{
+    public Image[] pages;
+    public int index = 0;
+    public Journal journal;
+    
+    public void Next()
+    {
+        pages[index].gameObject.SetActive(false);
+        index++;
+        if (index == 1)
+            {
+                journal.Inventory();
+            }
+        if (index == 2)
+            {
+                journal.GuestList(null);
+                journal.GuestTab();
+            }
+        pages[index].gameObject.SetActive(true);
+    }
+
+    public void prev()
+    {
+        pages[index].gameObject.SetActive(false);
+        index--;
+        if (index > 0)
+        {
+            if (index == 1)
+            {
+                journal.Inventory();
+            }
+            if (index == 2)
+            {
+                journal.GuestList(null);
+                journal.GuestTab();
+            }
+            pages[index].gameObject.SetActive(true);
+        }
+        else pages[0].gameObject.SetActive(true);
+    }
+   
+}
